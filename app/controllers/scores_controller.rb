5893e98fc0881c367c73453aa2af5c6f5d325c54
GAKU <gemini.05211124@gmail.com>
2018-08-16 16:37:01 +0900

    送信した値の保存など

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,7 +1,21 @@
 class ScoresController < ApplicationController
     def new
-        @player =Player.new 
+        @player = Player.new 
     end
+
     def create
+        @player = Player.new(player_params)
+        if @player.save
+            redirect_to root_path, notice: '登録が完了しました'
+        else
+            flash.now[:alert] = "登録に失敗しました"
+            render :new
+        end
+
+    end
+
+    private
+    def player_params
+        params.require(:player).permit(:baseline_player, :net_player,:baseline_player_opponent,:net_player_opponent)
     end
 end
