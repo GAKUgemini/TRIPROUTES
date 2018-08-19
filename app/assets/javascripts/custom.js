775060c94699c9162afad4e940c0d8e70e83b39a
GAKU <gemini.05211124@gmail.com>
2018-08-19 14:50:51 +0900

    JQueryの適応

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,17 +2,34 @@
     $("h2").text("jQuery稼働テスト(稼働中)");
 
     var flg = "off"; 
-    $(".cell").click(function(){
+    $(".cell1-1").click(function(){
         if(flg == "off"){
             $(this).text("○");
             flg = "on";
-            $(".opponent_cell").text("×");
+            $(".opponent_cell1-1").text("×");
+            $(".points").text("1")
+            $(".opponent_points").text("0")
         }else{
             $(this).text("×");
             flg = "off";
-            $(".opponent_cell").text("○");
+            $(".opponent_cell1-1").text("○");
+            $(".points").text("0")
+            $(".opponent_points").text("1")
         }
     });
+
+    $(".cell1-2").click(function(){
+        if(flg == "off"){
+            $(this).text("○");
+            flg = "on";
+            $(".opponent_cell1-2").text("×");
+        }else{
+            $(this).text("×");
+            flg = "off";
+            $(".opponent_cell1-2").text("○");
+        }
+    });
+
 
     $(".select_service").click(function(){
         $(".select_recive").hide();
