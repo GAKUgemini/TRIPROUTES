3e7c1bd6698d5dd2dfcd8d40752c23f7dfc346ce
GAKU <gemini.05211124@gmail.com>
2018-08-20 22:26:48 +0900

    Javascriptの編集

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,36 +1,34 @@
+  
   $(document).ready(function() {
-    $("h2").text("jQuery稼働テスト(稼働中)");
-
+    $("h2").text("稼働中");
+    var gameset = "off";
     var flg = "off"; 
-    $(".cell1-1").click(function(){
+    var game_count = [1,2,3,4,5,6,7];
+    var point_count = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+    var i = 0;
+    var j = 0;
+    $(".next-button").click(function(){
+       var prejflag = 1;
+       j = j + prejflag;
+    });
+    $(".cell"+game_count[i]+"-"+point_count[j]).click(function(){
         if(flg == "off"){
             $(this).text("○");
             flg = "on";
-            $(".opponent_cell1-1").text("×");
+            $(".opponent_cell"+game_count[i]+"-"+point_count[j]).text("×");
             $(".points").text("1")
             $(".opponent_points").text("0")
         }else{
             $(this).text("×");
             flg = "off";
-            $(".opponent_cell1-1").text("○");
+            $(".opponent_cell"+game_count[i]+"-"+point_count[j]).text("○");
             $(".points").text("0")
             $(".opponent_points").text("1")
         }
     });
-
-    $(".cell1-2").click(function(){
-        if(flg == "off"){
-            $(this).text("○");
-            flg = "on";
-            $(".opponent_cell1-2").text("×");
-        }else{
-            $(this).text("×");
-            flg = "off";
-            $(".opponent_cell1-2").text("○");
-        }
-    });
-
-
+    
+   
+    
     $(".select_service").click(function(){
         $(".select_recive").hide();
         $(".opponent_select_service").hide();
