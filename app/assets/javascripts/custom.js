c06c8b95dd4fb69d0512f58a1fcf276776cdbea4
GAKU <gemini.05211124@gmail.com>
2018-08-22 15:58:07 +0900

    jqueryによるポイントの動的変化

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,35 +7,56 @@
     var point_count_cel = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
     var points = 0;
     var opponent_points = 0;
-    var one_click = "off";
     var i = 0;
     var j = 0;
+    var first_click = "off";
+    var opponent_first_click = "off";
     $(".next-button").click(function(){
         var j_increment = 1;
         j = j+j_increment;
     });
     $(".save-button").click(function(){
-        one_click = "off";
+        first_click = "off";
+        opponent_first_click = "off";
     });
 
     $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).click(function(){
         if(flg == "off"){
             $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("○");
             flg = "on";
+            $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).addClass("circle");
             $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("×");
-            if(one_click == "off"){
-                var one_point = 1;
-                one_click = "on";
-                points = points+one_point;
+            points = $(".circle").length;
+            if(first_click == "off"){
+                $(".points").text(points);
+                $(".opponent_points").text(opponent_points);
+                first_click = "on";
             }
-            $(".points").text(points)
-            $(".opponent_points").text(opponent_points)
+            else{
+                $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).removeClass("opponent_circle");
+                opponent_points = $(".opponent_circle").length;
+                $(".points").text(points);
+                $(".opponent_points").text(opponent_points);
+            }
         }else{
-            $(this).text("×");
+            $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("×");
             flg = "off";
             $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("○");
-            $(".points").text("0")
-            $(".opponent_points").text("1")
+            $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).addClass("opponent_circle");
+            opponent_points = $(".opponent_circle").length;
+            if(opponent_first_click == "off"){
+                $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).removeClass("circle");
+                points = $(".circle").length;
+                $(".points").text(points);
+                $(".opponent_points").text(opponent_points);
+                opponent_first_click = "on";
+            }
+            else{
+                $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).removeClass("circle");
+                points = $(".circle").length;
+                $(".points").text(points);
+                $(".opponent_points").text(opponent_points);
+            }
         }
     });
     
