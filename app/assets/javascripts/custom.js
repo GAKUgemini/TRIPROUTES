48d5afb0d94e8ecfa61642dbeda07334c7773023
GAKU <gemini.05211124@gmail.com>
2018-08-22 13:42:57 +0900

    saveボタンの追加による値の動的変化表示

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,25 +3,37 @@
     $("h2").text("稼働中");
     var gameset = "off";
     var flg = "off"; 
-    var game_count = [1,2,3,4,5,6,7];
-    var point_count = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+    var game_count_cel = [1,2,3,4,5,6,7];
+    var point_count_cel = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
+    var points = 0;
+    var opponent_points = 0;
+    var one_click = "off";
     var i = 0;
     var j = 0;
     $(".next-button").click(function(){
         var j_increment = 1;
         j = j+j_increment;
     });
-    $(".cell"+game_count[i]+"-"+point_count[j]).click(function(){
+    $(".save-button").click(function(){
+        one_click = "off";
+    });
+
+    $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).click(function(){
         if(flg == "off"){
-            $(".cell"+game_count[i]+"-"+point_count[j]).text("○");
+            $(".cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("○");
             flg = "on";
-            $(".opponent_cell"+game_count[i]+"-"+point_count[j]).text("×");
-            $(".points").text("1")
-            $(".opponent_points").text("0")
+            $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("×");
+            if(one_click == "off"){
+                var one_point = 1;
+                one_click = "on";
+                points = points+one_point;
+            }
+            $(".points").text(points)
+            $(".opponent_points").text(opponent_points)
         }else{
             $(this).text("×");
             flg = "off";
-            $(".opponent_cell"+game_count[i]+"-"+point_count[j]).text("○");
+            $(".opponent_cell"+game_count_cel[i]+"-"+point_count_cel[j]).text("○");
             $(".points").text("0")
             $(".opponent_points").text("1")
         }
